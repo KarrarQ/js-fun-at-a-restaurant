1f119e47640e4b3b3654227be29f49a3bda06e5d
Karrar Qasim <Karawr1996@gmail.com>
2022-10-12 19:42:48 -0600

    Finished meal test

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,44 @@
-function nameMenuItem() {
+function nameMenuItem(menuItemName) {
+  return `Delicious ${menuItemName}`
+};
 
+function createMenuItem(name, price, type) {
+  return {
+    name: name,
+    price: price,
+    type: type
+  }
+};
+
+function addIngredients(food, ingredients) {
+  if(ingredients.includes(food) === false) {
+  ingredients.push(food)
+  }
 }
 
+function formatPrice(initialPrice) {
+  return '$' + initialPrice
+}
+
+function decreasePrice(decreasedPrice) {
+  return decreasedPrice - (decreasedPrice * .1)
+}
+
+function createRecipe(title, ingredients, type) {
+  return {
+    title: title,
+    ingredients: ingredients,
+    type: type
+  }
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
