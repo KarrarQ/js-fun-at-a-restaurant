0fadfe00f00d561983fba87b4178bc11c058892f
Karrar Qasim <Karawr1996@gmail.com>
2022-10-13 16:27:41 -0600

    Finished order test

--- src/order.js
+++ src/order.js
@@ -1,7 +1,38 @@
+function takeOrder(orders, deliveryOrders) {
+if(deliveryOrders.length < 3) {
+   deliveryOrders = deliveryOrders.push(orders)
+  } 
+}
+
+function refundOrder(orderNum, deliveryOrders) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if(orderNum === deliveryOrders[i].orderNumber) {
+      deliveryOrders.splice(i, 1)
+    }
+  }
+}
+
+function listItems(deliveryOrders) {
+  var listItem = [];
+  for(var i = 0; i < deliveryOrders.length; i++) {
+     listItem.push(deliveryOrders[i].item)
+  } 
+    return listItem.join(', ')
+}
+
+function searchOrder(deliveryOrders, orderItem) {
+  var found = false
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if(deliveryOrders[i].item === orderItem) {
+      found = true 
+    }
+  }
+  return found
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
