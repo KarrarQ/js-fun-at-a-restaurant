68f565c0f74ff59724efbb64765460b5ac68740b
Karrar Qasim <Karawr1996@gmail.com>
2022-10-15 19:57:59 -0600

    Finished chef test

--- src/Chef.js
+++ src/Chef.js
@@ -1,6 +1,21 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant) {
+    this.name = name;
+    this.restaurant = restaurant;
+  }
+  greetCustomer(name, foodItem) {
+    if(foodItem === true){
+      return `Good morning, ${name}!`
+     } else {
+      return `Hello, ${name}!`
+    }
+  }
+  checkForFood(foodItem) {
+    if(foodItem.name === "Quiche" || foodItem.name === "Kouign Amann") {
+      return `Sorry, we aren't serving ${foodItem.name} today.`
+    } else {
+      return `Yes, we're serving ${foodItem.name} today!`
+    }
   }
 }
 
